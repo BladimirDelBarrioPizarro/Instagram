c0ed39d590d0c363abadac2effe0ff12e29416da
bladimir.delbarrio <bladimaltego@gmail.com>
2020-07-01 15:20:37 +0200

    feat:added title.component

--- instagram/src/App.tsx
+++ instagram/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../src/modules/card/components/card.component'
+import Title from '../src/modules/card/components/title.component'
 import Container from '../src/modules/container/components/container.component'
 import Input from '../src/modules/input/components/input.component'
 import Button from '../src/modules/button/components/button.component'
@@ -8,10 +9,12 @@ import Button from '../src/modules/button/components/button.component'
 const App = () => {
   return (
     <Container>
-      <Card></Card>
+      <Card>
+        <Title/>
+      </Card>
       <Input placeholder='Email' label="Email"></Input>
       <Input placeholder='Password' label="Password"></Input>
-      <Button>Enviar</Button>
+      <Button></Button>
     </Container>
   );
 }
---
+++ instagram/src/modules/card/components/title.component.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Title = (props:any) => {
+
+    const styleTitle = {
+       color:'#555',
+       
+    }
+    return(
+        <h2 style={styleTitle}>Login</h2>
+    )
+}
+
+export default Title;
