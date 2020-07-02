e0e90138d9826a0437ed0428d806d9002625336b
bladimir.delbarrio <bladimaltego@gmail.com>
2020-07-02 09:11:40 +0200

    feat: added registry.component

--- instagram/src/modules/registry/components/registry.component.tsx
+++ instagram/src/modules/registry/components/registry.component.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Card from '../../card/components/card.component'
+import Input from '../../input/components/input.component'
+import Button from '../../button/components/button.component'
 
 const Registry = () => {
+
+    const linkStyle = {
+      textAlign:'center',
+      width:'100%',
+      marginLeft:'90px'
+    } as React.CSSProperties
+
+    
+    const styleh2 = {
+        color:'#555',
+ 
+     }
+
     return(
-        <div>Registry</div>
+      <div>  
+        <h2 style={styleh2}>Registry</h2>
+        <Input placeholder='Email' label="Email"></Input>
+        <Input placeholder='Password' label="Passw"></Input>
+        <Button block={true}></Button>
+        <Link style={linkStyle} to={"/registry"}>Login in</Link>
+    </div>  
     )
 }
 
