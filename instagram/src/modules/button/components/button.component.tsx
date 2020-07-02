e8f980d712850215c8af7c185a9a7ebae407c92c
bladimir.delbarrio <bladimaltego@gmail.com>
2020-07-02 08:50:25 +0200

    feat:added linkStyle in Link

--- instagram/src/modules/button/components/button.component.tsx
+++ instagram/src/modules/button/components/button.component.tsx
@@ -2,23 +2,27 @@ import React from 'react'
 
 
 interface IButtonProps {
-   
+   block?:boolean
 }
 
 const Button = (props:IButtonProps) => {
     
     
-    const buttonStyle = {
+    const buttonStyle = (block:boolean) => ({
         backgroundColor:'#1FE9B8',
         border:'0px',
         borderRadius:'4px',
         padding:'10px 15px',
         marginTop:'4px',
-        color:'#fff'
-    }
+        marginBottom:'10px',
+        color:'#fff',
+        width:block?'100%' :undefined
+    })
+
+    const {block = false} = props
 
     return(
-        <button style={buttonStyle}>Enviar</button>
+        <button style={buttonStyle(block)}>Enviar</button>
     )
 }
 
--- instagram/src/modules/login/components/login.components.tsx
+++ instagram/src/modules/login/components/login.components.tsx
@@ -5,13 +5,20 @@ import Input from '../../input/components/input.component'
 import Button from '../../button/components/button.component'
 
 const Login = () => {
+
+    const linkStyle = {
+      textAlign:'center',
+      width:'100%',
+      marginLeft:'90px'
+    } as React.CSSProperties
+
     return(
       <div>  
         <Card/>
         <Input placeholder='Email' label="Email"></Input>
         <Input placeholder='Password' label="Passw"></Input>
-        <Button></Button>
-        <Link to={"/registry"}>Registry</Link>
+        <Button block={true}></Button>
+        <Link style={linkStyle} to={"/registry"}>Registry</Link>
     </div>  
     )
 }
